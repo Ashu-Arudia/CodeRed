7f2494b10ca7fd9fc7e38907515bec00446a6c4a
Ashu-Arudia <aaravarudi@gmail.com>
2025-08-16 21:25:28 +0530

    added logics

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -21,7 +21,7 @@ passport.use(
         }
 
         const insertResult = await pool.query(
-          `INSERT INTO users (google_id, email, is_verified, first_name, second_name, profile_picture)
+          `INSERT INTO users (google_id, email, is_verified, first_name, last_name, profile_image_url)
            VALUES ($1, $2, $3, $4, $5, $6)
            RETURNING *`,
           [
