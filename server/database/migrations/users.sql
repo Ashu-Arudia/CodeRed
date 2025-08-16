7bb17bb951092adb2c943f7cdcb4c0dfefd2fc68
Himanshu <himanshu.k19102004@gmail.com>
2025-08-16 20:59:19 +0530

    Added new script

--- server/database/migrations/users.sql
+++ server/database/migrations/users.sql
@@ -10,6 +10,7 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     
     -- Authentication & Identity
+    
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
